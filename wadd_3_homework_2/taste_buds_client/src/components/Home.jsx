f2dd9767fadd619ece1c0f9bccae37edc7c4db0a
HoomanMinoo <hoomanminoosepehr@gmail.com>
2023-07-07 23:11:36 -0700

    welcome page cover and navbar design is done for no

---
+++ wadd_3_homework_2/taste_buds_client/src/components/Home.jsx
@@ -0,0 +1,17 @@
+
+
+function Home() {
+
+    return (
+        <div className='home-main'>
+            <div className="container">
+                <h2>Welcome to Taste Buds</h2>
+                <p>In this WebSite you can find the recipes to make your desired food.</p>
+                <p>Or you can share the recipes you now!</p>
+            </div>
+        </div>
+    );
+
+};
+
+export default Home;
