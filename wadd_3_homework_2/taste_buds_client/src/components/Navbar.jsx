e4a755589effb4a9fa1f3f7ce8b21cb42d17a45e
HoomanMinoo <hoomanminoosepehr@gmail.com>
2023-07-08 23:04:49 -0700

    still working on fading the bottom of navbar

--- wadd_3_homework_2/taste_buds_client/src/components/Navbar.jsx
+++ wadd_3_homework_2/taste_buds_client/src/components/Navbar.jsx
@@ -5,10 +5,12 @@ function Navbar() {
 
     return (
         <div className='nav-main'>
-            <NavLink to={'/'}>Home</NavLink>
-            <NavLink to={'/recipes'}>Recipes</NavLink>
-            <NavLink to={'/sign-in'}>Sign In</NavLink>
-            <NavLink to={'/sign-up'}>Sign Up</NavLink>
+            <div className='nav-second'>
+                <NavLink to={'/'}>Home</NavLink>
+                <NavLink to={'/recipes'}>Recipes</NavLink>
+                <NavLink to={'/sign-in'}>Sign In</NavLink>
+                <NavLink to={'/sign-up'}>Sign Up</NavLink>
+            </div>
         </div>
     )
 };
