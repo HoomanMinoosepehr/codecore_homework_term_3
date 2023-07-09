615c533020955f2aade31f649e6cdb108a2653f3
HoomanMinoo <hoomanminoosepehr@gmail.com>
2023-07-08 23:03:37 -0700

    adding some setting for the session, because I`ve strted the rails application with using --api

--- wadd_3_homework_2/taste_buds_api/config/application.rb
+++ wadd_3_homework_2/taste_buds_api/config/application.rb
@@ -35,5 +35,16 @@ module TasteBudsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+  
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_coookie_name', expire_after: 30.days
+
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+      end
+    end
+    
   end
 end
