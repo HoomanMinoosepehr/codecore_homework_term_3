8f7ec4ca579ef1ae4434c850e34c5e154edff4ef
HoomanMinoo <hoomanminoosepehr@gmail.com>
2023-07-09 14:44:40 -0700

    fading the bottom of the navbar and top of the current page is done

--- wadd_3_homework_2/taste_buds_client/src/components/Home.jsx
+++ wadd_3_homework_2/taste_buds_client/src/components/Home.jsx
@@ -4,10 +4,12 @@ function Home() {
 
     return (
         <div className='home-main'>
-            <div className="container">
-                <h2>Welcome to Taste Buds</h2>
-                <p>In this WebSite you can find the recipes to make your desired food.</p>
-                <p>Or you can share the recipes you now!</p>
+            <div className="home-second">
+                <div className="container">
+                    <h2>Welcome to Taste Buds</h2>
+                    <p>In this WebSite you can find the recipes to make your desired food.</p>
+                    <p>Or you can share the recipes you now!</p>
+                </div>
             </div>
         </div>
     );
