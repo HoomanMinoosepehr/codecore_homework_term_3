8bec1a217ae37f8735dad67ec3fd2b367824357a
HoomanMinoo <hoomanminoosepehr@gmail.com>
2023-08-02 20:48:25 -0700

    setIntervals custom hook added to the project from freeCodeCamp git repo

--- wadd_3_homework_3/react_tetris/src/components/hooks/useStage.js
+++ wadd_3_homework_3/react_tetris/src/components/hooks/useStage.js
@@ -45,6 +45,6 @@ export const useStage = (player, resetPlayer) => {
         setStage( (prev) => updateStage(prev));
     }, [player, resetPlayer])
 
-    return [stage, setStage];
+    return [stage, setStage, rowsCleared];
 
 }
