8a5fcf629f8a5e51e62c28049dce1b922f360b29
HoomanMinoo <hoomanminoosepehr@gmail.com>
2023-08-02 20:17:53 -0700

    prevent the block from rotating near other blocks or near the sidewall

--- wadd_3_homework_3/react_tetris/src/components/hooks/usePlayer.js
+++ wadd_3_homework_3/react_tetris/src/components/hooks/usePlayer.js
@@ -1,6 +1,6 @@
 import { useCallback, useState } from "react";
 import { TETROMINOS, randomTetromino } from "../../tetrominos";
-import { STAGE_WIDTH } from "../../gameHelper";
+import { STAGE_WIDTH, checkCollision } from "../../gameHelper";
 
 
 export function usePlayer(props) {
@@ -17,10 +17,22 @@ export function usePlayer(props) {
     }
 
     const playerRotate = (stage, dir) => {
-         const clonedPlayer = JSON.parse(JSON.stringify(player));
-         clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+        const clonedPlayer = JSON.parse(JSON.stringify(player));
+        clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
 
-         setPlayer(clonedPlayer);
+        const pos = clonedPlayer.pos.x;
+        let offset = 1;
+        while (checkCollision(clonedPlayer, stage, { x:0 , y:0 })) {
+            clonedPlayer.pos.x += offset;
+            offset = -(offset + (offset > 0 ? 1 : -1));
+            if (offset > clonedPlayer. tetromino[0].length) {
+                rotate(clonedPlayer.tetromino, -dir);
+                clonedPlayer.pos.x = pos;
+                return; 
+            }
+        }
+
+        setPlayer(clonedPlayer);
     }
 
     const updatePlayerPos = ({ x, y, collided }) => {
